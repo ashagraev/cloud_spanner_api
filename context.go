f92a3bcd5700a375b346b13bd4293bfe8b60f4ac
Aleksey Shagraev <alex-shagraev@ya.ru>
2020-09-14 02:00:49 +0300

    better command line option descriptions

--- context.go
+++ context.go
@@ -10,16 +10,16 @@ func PrepareContext() context.Context {
 	// os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "C:\\Users\\alex-\\OneDrive\\Desktop\\google-cloud-key.json")
 
 	verbose := false
-	flag.BoolVar(&verbose, "verbose", verbose, "be verbose")
+	flag.BoolVar(&verbose, "verbose", verbose, "turn on loading process logging")
 
 	noTablesExport := false
 	flag.BoolVar(&noTablesExport, "no-tables", noTablesExport, "do not export detailed tables information")
 
 	noGoroutines := false
-	flag.BoolVar(&noGoroutines, "no-goroutines", noGoroutines, "do not use goroutines for databases load")
+	flag.BoolVar(&noGoroutines, "no-goroutines", noGoroutines, "do not use goroutines while loading databases information")
 
 	jsonLines := false
-	flag.BoolVar(&jsonLines, "json-lines", jsonLines, "export each database information on in a separate json line")
+	flag.BoolVar(&jsonLines, "json-lines", jsonLines, "export each database information on a separate json line")
 
 	jsonPretty := false
 	flag.BoolVar(&jsonPretty, "pretty", jsonPretty, "prettify json output")
